b0a3923f2cd8d796f8a7e73f8f05df4898076453
jawad qasim <jawadqasim99@gmail.com>
2024-01-31 13:37:31 +0500

    Starad air bnb website

--- models/listing/listing.js
+++ models/listing/listing.js
@@ -13,7 +13,7 @@ const listSehcma= mongoose.Schema({
     image:{
         type:String,
         default:"https://unsplash.com/photos/a-couple-of-people-standing-on-top-of-a-hill-5jBY78hkqto",
-        set:(v)=> v === ""? "https://unsplash.com/photos/a-couple-of-people-standing-on-top-of-a-hill-5jBY78hkqto":v
+        set:(v)=> v === " "? "https://unsplash.com/photos/a-couple-of-people-standing-on-top-of-a-hill-5jBY78hkqto":v
     },
     price:Number,
     location:String,
